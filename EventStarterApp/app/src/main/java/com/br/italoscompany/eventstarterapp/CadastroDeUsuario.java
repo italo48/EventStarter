bcb34ac09b44e08506f1332eba971b5d8fe0d68f
luanderson <luandersondjd@gmail.com>
2019-10-22 21:51:54 -0300

    modificado

---
+++ EventStarterApp/app/src/main/java/com/br/italoscompany/eventstarterapp/CadastroDeUsuario.java
@@ -0,0 +1,15 @@
+package com.br.italoscompany.eventstarterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class CadastroDeUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_de_usuario);
+    }
+}
