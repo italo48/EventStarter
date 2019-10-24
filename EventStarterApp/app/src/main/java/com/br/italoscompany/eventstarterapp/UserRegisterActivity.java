651d4996c6604a8c458b08f5eaf97e49f0cda569
luanderson <luandersondjd@gmail.com>
2019-10-24 11:16:26 -0300

    alterações

--- EventStarterApp/app/src/main/java/com/br/italoscompany/eventstarterapp/UserRegisterActivity.java
+++ EventStarterApp/app/src/main/java/com/br/italoscompany/eventstarterapp/UserRegisterActivity.java
@@ -3,13 +3,12 @@ package com.br.italoscompany.eventstarterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 
 public class UserRegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro_de_usuario);
+        setContentView(R.layout.activity_user_register);
     }
 }
